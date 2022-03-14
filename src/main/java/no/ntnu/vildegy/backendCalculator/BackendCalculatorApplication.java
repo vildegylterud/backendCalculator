705e6e96f6826f6bdb9aefb09b49a6e7ba003aef
Vilde Gylterud <vildegylterud@outlook.com>
2022-03-14 15:19:31 +0100

    first commit

---
+++ src/main/java/no/ntnu/vildegy/backendCalculator/BackendCalculatorApplication.java
@@ -0,0 +1,14 @@
+package no.ntnu.vildegy.backendCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCalculatorApplication.class, args);
+	}
+
+}
